01cfe0ea41f457ebe104325dfbbc8656955a6bfe
fhmiibrhimdev <fahmidev.ibrahim@gmail.com>
2023-04-07 05:43:16 +0700

    Add Framework TailwindCSS

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'tw-',
+  content: [
+    './src/**/*.js',
+    './src/**/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
